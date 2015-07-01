818fffb5aee784c0390e256107a96d479bc3ca02
PixelBreeze <thepixelbreeze@gmail.com>
2015-07-02 00:54:21 +0300

    Update PSmain.js

--- PSmain.js
+++ PSmain.js
@@ -3,3 +3,4 @@
 //The button ??
 $('head').append('<link rel="stylesheet" href="https://rawgit.com/PixelBreeze/PotatoScript/master/styles.css" type="text/css"/>');
 $('body').append('<div id="tbutton"></div>');
+$('#tbutton').draggable();
