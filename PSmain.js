5b905bc9acc10c9ee08e9f47f79a18bfaf8eddde
PixelBreeze <thepixelbreeze@gmail.com>
2015-07-01 19:53:48 +0300

    Update PSmain.js

--- PSmain.js
+++ PSmain.js
@@ -1,5 +1,5 @@
 //PotatoScript
 
 //The button ??
-$('<link rel="stylesheet" href="https://raw.githubusercontent.com/PixelBreeze/PotatoScript/master/styles.css" type="text/css">').appendTo("head");
+$('<link rel="stylesheet" href="https://rawgit.com/PixelBreeze/PotatoScript/master/styles.css" type="text/css">').appendTo("head");
 $('body').append('<div id="tbutton"></div>');
