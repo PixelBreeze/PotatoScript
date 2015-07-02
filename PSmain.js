8952672542373475b968258e7e2b2786393b86aa
PixelBreeze <thepixelbreeze@gmail.com>
2015-07-02 18:15:05 +0300

    Update PSmain.js

--- PSmain.js
+++ PSmain.js
@@ -1,10 +1,8 @@
 //PotatoScript
 
 //The button ??
-$('head').append('<link rel="stylesheet" href="https://rawgit.com/PixelBreeze/PotatoScript/master/styles.css" type="text/css"/> <script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>');
+$('head').append('<link rel="stylesheet" href="https://rawgit.com/PixelBreeze/PotatoScript/master/styles.css" type="text/css"/>');
+$('head').append('<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>');
+$('head').append('<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.9.1/jquery-ui.min.js"></script>');
+$('head').append('$(document).ready(function() { $("#tbutton").draggable();});');
 $('body').append('<div id="tbutton"></div>');
-$('head').append('<script type="text/javascript">
-    jQuery(document).ready(function() {
-        $('#tbutton').draggable();
-    });
-</script>');
