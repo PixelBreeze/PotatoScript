561eba40c96b893e96d8202b23e420a7c59c823b
PixelBreeze <thepixelbreeze@gmail.com>
2015-07-04 01:17:55 +0300

    Update PSmain.js

--- PSmain.js
+++ PSmain.js
@@ -2,7 +2,28 @@
 
 //The button ??
 $('head').append('<link rel="stylesheet" href="https://rawgit.com/PixelBreeze/PotatoScript/master/styles.css" type="text/css"/>');
-$('head').append('<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>');
-$('head').append('<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.9.1/jquery-ui.min.js"></script>');
-$('head').append('$(document).ready(function() { $("#tbutton").draggable();});');
-$('body').append('<div id="tbutton"></div>');
+$.getScript('https://ajax.googleapis.com/ajax/libs/jqueryui/1.9.1/jquery-ui.min.js').done(function(data){
+$('body').append('<div id="tbutton"><img id="potatoimg" src="http://pngimg.com/upload/potato_PNG7078.png"/></div>');
+$("#tbutton").draggable(); });
+
+var next_move = "expand";
+$(document).ready(function (){
+$("#tbutton").click(function()
+{
+    console.log(next_move);
+    var css = {};
+    if (next_move == "expand"){
+        css = {
+            width: '200px'
+        };
+        next_move = "shrink";
+    } else {
+        css = {
+            width: '50px'
+        };     
+        console.log('hi');
+        next_move = "expand";
+    }
+    $(this).animate(css, 200);
+});
+});
