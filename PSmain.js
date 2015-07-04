9be1fccc4f24e5f90db36a05321170fb15ef43f2
PixelBreeze <thepixelbreeze@gmail.com>
2015-07-04 12:37:47 +0300

    Update PSmain.js

--- PSmain.js
+++ PSmain.js
@@ -1,25 +1,19 @@
-//PotatoScript
 
-//The button //FIRST PART
+//Potato Script
 $('head').append('<link rel="stylesheet" href="https://rawgit.com/PixelBreeze/PotatoScript/master/styles.css" type="text/css"/>');
-$.getScript('https://ajax.googleapis.com/ajax/libs/jqueryui/1.9.1/jquery-ui.min.js').done(function(data){
-$('body').append('<div id="tbutton"><img id="potatoimg" src="http://pngimg.com/upload/potato_PNG7078.png"/></div>');
-$("#tbutton").draggable(); });
-
-//Expands the button //SECOND PART
-var next_move = "expand";
-$("#tbutton").click(function (){
-    var css = {};
-    if (next_move == "expand"){
-        css = {
-            width: '200px'
-        };
-        next_move = "shrink";
-    } else {
-        css = {
-            width: '50px'
-        };  
-        next_move = "expand";
-    }
-    $(this).animate(css, 200);
+$.getScript('https://ajax.googleapis.com/ajax/libs/jqueryui/1.9.1/jquery-ui.min.js').done(function(data) {
+    $('body').append('<div id="tbutton"><img id="potatoimg" src="http://pngimg.com/upload/potato_PNG7078.png"/></div>');
+    $("#tbutton").draggable();
+    $("#tbutton").click(function() {
+        var css = {};
+        var next_move = "expand";
+        if (this.next_move == "expand") {
+            css = { width: '200px' };
+            this.next_move = "shrink";
+        } else {
+            css = { width: '50px' };
+            this.next_move = "expand";
+        }
+        $(this).animate(css, 200);
+    });
 });
