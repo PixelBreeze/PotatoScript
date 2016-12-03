6b7f2077d8689a5c67c27fdbe299b5374fdb769c
PixelBreeze <thepixelbreeze@gmail.com>
2016-12-03 20:20:15 +0200

    Update PSmain.js

--- PSmain.js
+++ PSmain.js
@@ -2,7 +2,7 @@
 //Potato Script
 $('head').append('<link rel="stylesheet" href="https://rawgit.com/PixelBreeze/PotatoScript/master/styles.css" type="text/css"/>');
 $.getScript('https://ajax.googleapis.com/ajax/libs/jqueryui/1.9.1/jquery-ui.min.js').done(function(data) {
-    $('body').append('<div id="tbutton"><img id="potatoimg" src="http://pngimg.com/upload/potato_PNG7078.png"/></div>');
+    $('body').append('<div id="tbutton"><div id="phbox"><img id="potatoimg" src="http://pngimg.com/upload/potato_PNG7078.png"/></div><div id="pcbox"></div></div>');
     $("#tbutton").draggable();
     $("#tbutton").click(function() {
         var css = {};
@@ -10,6 +10,7 @@ $.getScript('https://ajax.googleapis.com/ajax/libs/jqueryui/1.9.1/jquery-ui.min.
         if (this.next_move == "expand") {
             css = { width: '200px' };
             this.next_move = "shrink";
+            
         } else {
             css = { width: '50px' };
             this.next_move = "expand";
