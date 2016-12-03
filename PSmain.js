209cd61edf2e5c007f06e84ad3487d7c0beb18ca
PixelBreeze <thepixelbreeze@gmail.com>
2016-12-03 21:17:40 +0200

    Update PSmain.js

--- PSmain.js
+++ PSmain.js
@@ -5,18 +5,19 @@ $.getScript('https://ajax.googleapis.com/ajax/libs/jqueryui/1.9.1/jquery-ui.min.
     $('body').append('<div id="tbutton"><div id="phbox"><img id="potatoimg" src="http://pngimg.com/upload/potato_PNG7078.png"/></div></div>');
     $('#tbutton').append('<div id="pcbox"></div>');
     $('#tbutton').draggable();
-    var pcbox = document.getElementsByTagName("pcbox");
     $("#phbox").click(function() {
         var css = {};
         var next_move = "expand";
         if (this.next_move == "expand") {
             css = { width: '200px' };
-            $('#pcbox').css = { display: 'block' };
             this.next_move = "shrink";            
         } else {
             css = { width: '50px' };
             this.next_move = "expand";
         }
         $(this).animate(css, 200);
+    });
+    $("#phbox").click(function(){
+        $("#pcbox").toggle();
     });
 });
