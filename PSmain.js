b97dfaca251771977ae241ff36ffc5bbe04b43a8
PixelBreeze <thepixelbreeze@gmail.com>
2016-12-04 00:58:13 +0200

    Update PSmain.js

--- PSmain.js
+++ PSmain.js
@@ -4,7 +4,7 @@ $('head').append('<link rel="stylesheet" href="https://rawgit.com/PixelBreeze/Po
 $.getScript('https://ajax.googleapis.com/ajax/libs/jqueryui/1.9.1/jquery-ui.min.js').done(function(data) {
    
     $('body').append('<div id="tbutton"><div id="phbox"><img id="potatoimg" src="http://pngimg.com/upload/potato_PNG7078.png"/></div></div>');
-    $('#tbutton').append('<div id="pcbox" style="display: none;"><div id="pgot" class="pibox"></div><div id="pbltheme" class="pibox"></div><div id="pbljunk" class="pibox"></div><div id="pskip" class="pibox"></div><div id="preload" class="pibox"></div><div id="pafk" class="pibox"></div><div id="pluul" class="pibox"></div></div>');
+    $('#tbutton').append('<div id="pcbox" style="display: none;"><div id="pgot" class="pibox">Got</div><div id="pbltheme" class="pibox">BL Theme</div><div id="pbljunk" class="pibox">BL Junk</div><div id="pskip" class="pibox">Skip</div><div id="preload" class="pibox">Reload</div><div id="pafk" class="pibox">AFK</div><div id="pluul" class="pibox">:luul:</div></div>');
     $('#tbutton').draggable();
     
     $("#phbox").click(function() {
