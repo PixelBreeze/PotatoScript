709b0b2c7b8b3ce59903dfd864210df2cba1594a
PixelBreeze <thepixelbreeze@gmail.com>
2016-12-04 12:52:53 +0200

    Update loader.js

--- loader.js
+++ loader.js
@@ -1,12 +1,10 @@
 $.getScript('https://code.radiant.dj/rs.min.js');
 if (psload === true) {
     $('#tbutton').remove();
-    var cleanup = function () {
-    delete window.;
-    delete window.;
-    };
+    delete window.next_move;
     $.getScript('https://rawgit.com/PixelBreeze/PotatoScript/master/PSmain.js');
 } else {
     $.getScript('https://rawgit.com/PixelBreeze/PotatoScript/master/PSmain.js');
+}
 
   
