f381af824f2230d94dde25d457584cdd968ec30e
PixelBreeze <thepixelbreeze@gmail.com>
2016-12-04 13:02:14 +0200

    Update PSmain.js

--- PSmain.js
+++ PSmain.js
@@ -1,6 +1,5 @@
 //Potato Script
-var psload = true;
-$('head').append('<link rel="stylesheet" href="https://rawgit.com/PixelBreeze/PotatoScript/master/styles.css" type="text/css"/>');
+$('head').append('<link id="pscss" rel="stylesheet" href="https://rawgit.com/PixelBreeze/PotatoScript/master/styles.css" type="text/css"/>');
 $.getScript('https://ajax.googleapis.com/ajax/libs/jqueryui/1.9.1/jquery-ui.min.js').done(function(data) {
    
     $('body').append('<div id="tbutton"><div id="phbox"><img id="potatoimg" src="http://pngimg.com/upload/potato_PNG7078.png"/></div></div>');
