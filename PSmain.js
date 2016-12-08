deab29cd9376453824c3a278d4fdcc0cef8fc19b
PixelBreeze <thepixelbreeze@gmail.com>
2016-12-08 16:59:55 +0200

    Update PSmain.js

--- PSmain.js
+++ PSmain.js
@@ -4,8 +4,11 @@ $.getScript('https://ajax.googleapis.com/ajax/libs/jqueryui/1.9.1/jquery-ui.min.
    
     $('body').append('<div id="tbutton"><div id="phbox"><img id="potatoimg" src="http://pngimg.com/upload/potato_PNG7078.png"/></div></div>');
     $('#tbutton').append('<div id="pcbox" style="display: none;"><div id="pgot" class="pibox">Got</div><div id="pbltheme" class="pibox">BL Theme</div><div id="pbljunk" class="pibox">BL Junk</div><div id="pskip" class="pibox">Skip</div><div id="preload" class="pibox">Reload</div><div id="pafk" class="pibox">AFK</div><div id="pluul" class="pibox">:luul:</div><div id="p420" class="pibox">420</div></div>');
+    $('body').append('<div id="confirmbox"><div id="confirmheader"><div id="cyes">Yes</div><div id="cno">No</div></div></div>');
     $('#tbutton').draggable();
+    $("#confirmbox").toggle();
     var next_move = 1;
+    var confirmed = false;
     $("#potatoimg").click(function() {
         if (next_move == 1) {
             $('#tbutton').animate({ 'width' : '273' });
@@ -16,6 +19,22 @@ $.getScript('https://ajax.googleapis.com/ajax/libs/jqueryui/1.9.1/jquery-ui.min.
         }
     //   $('#tbutton').animate({width: '273px'});
     }); 
+   
+   function confirm() {
+      $("#confirmbox").toggle();
+      $("#cyes").click(function() {
+         confirmed = true;
+         $("#confirmbox").toggle();
+      });
+      $("#cno").click(function() {
+         confirmed = false;
+         $("#confirmbox").toggle();
+      });
+   }
+   $(".pibox").click(function() {
+      confirm();
+   });
+   
    $("#potatoimg").click(function() {
       $("#pcbox").toggle(); 
    });
@@ -23,7 +42,9 @@ $.getScript('https://ajax.googleapis.com/ajax/libs/jqueryui/1.9.1/jquery-ui.min.
         API.sendChat('I got it.'); 
     });
     $("#pskip").click(function(){
-        API.sendChat('!skip'); 
+       if (confirmed === true) {
+         API.sendChat('!skip'); 
+       }
     });
     $("#pbltheme").click(function(){
         API.sendChat('!bl theme'); 
@@ -38,7 +59,9 @@ $.getScript('https://ajax.googleapis.com/ajax/libs/jqueryui/1.9.1/jquery-ui.min.
         API.sendChat('!afk'); 
     });
     $("#pluul").click(function(){
-        API.sendChat(':luul:'); 
+       if (confirmed === true) {
+         API.sendChat(':luul:'); 
+       }
     });
    $("#p420").click(function(){
         API.sendChat('4:20'); 
