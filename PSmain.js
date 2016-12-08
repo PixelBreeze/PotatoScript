2d7e6b4db72a61baa76ed60f3e05646ac2cdccab
PixelBreeze <thepixelbreeze@gmail.com>
2016-12-08 17:24:10 +0200

    Update PSmain.js

--- PSmain.js
+++ PSmain.js
@@ -4,7 +4,6 @@ $.getScript('https://ajax.googleapis.com/ajax/libs/jqueryui/1.9.1/jquery-ui.min.
    
     $('body').append('<div id="tbutton"><div id="phbox"><img id="potatoimg" src="http://pngimg.com/upload/potato_PNG7078.png"/></div></div>');
     $('#tbutton').append('<div id="pcbox" style="display: none;"><div id="pgot" class="pibox">Got</div><div id="pbltheme" class="pibox">BL Theme</div><div id="pbljunk" class="pibox">BL Junk</div><div id="pskip" class="pibox">Skip</div><div id="preload" class="pibox">Reload</div><div id="pafk" class="pibox">AFK</div><div id="pluul" class="pibox">:luul:</div><div id="p420" class="pibox">420</div></div>');
-    $('body').append('<div id="confirmbox"><div id="confirmheader">Are you sure?</div><div id="cyes" class="cbox">Yes</div><div id="cno" class="cbox">No</div></div>');
     $('#tbutton').draggable();
     $("#confirmbox").toggle();
     var next_move = 1;
@@ -19,20 +18,8 @@ $.getScript('https://ajax.googleapis.com/ajax/libs/jqueryui/1.9.1/jquery-ui.min.
         }
     //   $('#tbutton').animate({width: '273px'});
     }); 
-   
-   function confirm() {
-      $("#confirmbox").toggle();
-      $("#cyes").click(function() {
-         confirmed = true;
-         $("#confirmbox").toggle();
-      });
-      $("#cno").click(function() {
-         confirmed = false;
-         $("#confirmbox").toggle();
-      });
-   }
    $(".pibox").click(function() {
-      confirm();
+      var confirmed = confirm("Are You Sure?");
    });
    
    $("#potatoimg").click(function() {
@@ -42,9 +29,7 @@ $.getScript('https://ajax.googleapis.com/ajax/libs/jqueryui/1.9.1/jquery-ui.min.
         API.sendChat('I got it.'); 
     });
     $("#pskip").click(function(){
-       if (confirmed === true) {
          API.sendChat('!skip'); 
-       }
     });
     $("#pbltheme").click(function(){
         API.sendChat('!bl theme'); 
@@ -59,9 +44,7 @@ $.getScript('https://ajax.googleapis.com/ajax/libs/jqueryui/1.9.1/jquery-ui.min.
         API.sendChat('!afk'); 
     });
     $("#pluul").click(function(){
-       if (confirmed === true) {
-         API.sendChat(':luul:'); 
-       }
+         API.sendChat(':luul:');      
     });
    $("#p420").click(function(){
         API.sendChat('4:20'); 
