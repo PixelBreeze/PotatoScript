bfddf809dee23cd19901ea24952bf7fecd3e3a58
PixelBreeze <thepixelbreeze@gmail.com>
2017-02-08 13:08:58 +0200

    Update PSmain.js

--- PSmain.js
+++ PSmain.js
@@ -2,19 +2,19 @@
 $('head').append('<link id="pscss" rel="stylesheet" href="https://rawgit.com/PixelBreeze/PotatoScript/master/styles.css" type="text/css"/>');
 $.getScript('https://ajax.googleapis.com/ajax/libs/jqueryui/1.9.1/jquery-ui.min.js').done(function(data) {
    
-    $('body').append('<div id="tbutton"><div id="phbox"><img id="potatoimg" src="http://pngimg.com/upload/potato_PNG7078.png"/></div></div>');
-    $('#tbutton').append('<div id="pcbox" style="display: none;"><div id="pgot" class="pibox">Got</div><div id="pbltheme" class="pibox pimportant">BL Theme</div><div id="pbljunk" class="pibox pimportant">BL Junk</div><div id="pblop" class="pibox pimportant">BL OP</div><div id="pskip" class="pibox pimportant">Skip</div><div id="preload" class="pibox">Reload</div><div id="pafk" class="pibox">AFK</div><div id="pluul" class="pibox">:luul:</div><div id="p420" class="pibox">420</div></div>');
-    $('#tbutton').draggable();
+    $('body').append('<div id="ptbutton"><div id="phbox"><img id="potatoimg" src="http://pngimg.com/upload/potato_PNG7078.png"/></div></div>');
+    $('#ptbutton').append('<div id="pcbox" style="display: none;"><div id="pgot" class="pibox">Got</div><div id="pbltheme" class="pibox pimportant">BL Theme</div><div id="pbljunk" class="pibox pimportant">BL Junk</div><div id="pblop" class="pibox pimportant">BL OP</div><div id="pskip" class="pibox pimportant">Skip</div><div id="preload" class="pibox">Reload</div><div id="pafk" class="pibox">AFK</div><div id="pluul" class="pibox">:luul:</div><div id="p420" class="pibox">420</div></div>');
+    $('#ptbutton').draggable();
     $("#confirmbox").toggle();
     var next_move = 1;
     var confirmed = false;
     var paction = " ";
     $("#potatoimg").click(function() {
         if (next_move == 1) {
-            $('#tbutton').animate({ 'width' : '273' });
+            $('#ptbutton').animate({ 'width' : '273' });
             next_move = 0;            
         } else {
-            $('#tbutton').animate({ 'width' : '50' }); 
+            $('#ptbutton').animate({ 'width' : '50' }); 
             next_move = 1;
         }
     //   $('#tbutton').animate({width: '273px'});
