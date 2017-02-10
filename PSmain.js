006f804dbf81471543ed1b029298ee24b8ab2e38
PixelBreeze <thepixelbreeze@gmail.com>
2017-02-10 21:05:55 +0200

    Update PSmain.js

--- PSmain.js
+++ PSmain.js
@@ -1,10 +1,8 @@
 //Potato Script
 $('head').append('<link id="pscss" rel="stylesheet" href="https://rawgit.com/PixelBreeze/PotatoScript/master/styles.css" type="text/css"/>');
-$.getScript('https://ajax.googleapis.com/ajax/libs/jqueryui/1.9.1/jquery-ui.min.js').done(function(data) {
    
     $('body').append('<div id="psbutton"><div id="phbox"><img id="potatoimg" src="http://pngimg.com/upload/potato_PNG7078.png"/></div></div>');
     $('#psbutton').append('<div id="pcbox" style="display: none;"><div id="pgot" class="pibox">Got</div><div id="pbltheme" class="pibox pimportant">BL Theme</div><div id="pbljunk" class="pibox pimportant">BL Junk</div><div id="pblop" class="pibox pimportant">BL OP</div><div id="pskip" class="pibox pimportant">Skip</div><div id="preload" class="pibox">Reload</div><div id="pafk" class="pibox">AFK</div><div id="pluul" class="pibox">:luul:</div><div id="p420" class="pibox">420</div></div>');
-    $('#psbutton').draggable();
     $("#confirmbox").toggle();
     var pnext_move = 1;
     var pconfirmed = false;
@@ -68,5 +66,4 @@ $.getScript('https://ajax.googleapis.com/ajax/libs/jqueryui/1.9.1/jquery-ui.min.
          executepaction();
       }
    });
-});
 
