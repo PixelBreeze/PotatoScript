8bf875740e0480c1d1b0d3d55fdb200ae51db696
PixelBreeze <thepixelbreeze@gmail.com>
2017-03-15 23:24:48 +0200

    Update loader.js

--- loader.js
+++ loader.js
@@ -3,4 +3,8 @@ $('#psbutton').remove();
 $('#pscss').remove();
 delete window.next_move;
 $.getScript('https://rawgit.com/PixelBreeze/PotatoScript/master/PSmain.js');
-  
+API.on(API.CHAT_COMMAND, function(data) { //loadnem for NotEnoughMemes script.
+    if (data.length > 0 && data.indexOf('/loadnem') == 0) {
+        $.getScript('https://rawgit.com/PixelBreeze/NotEnoughMemes/master/NEMuserscript.js');
+    }
+});
